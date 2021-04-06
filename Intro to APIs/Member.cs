0caa2a818662f0a652e2c235b2706162a2496e60
KZarzour <49167803+KZarzour@users.noreply.github.com>
2021-04-06 15:28:51 +0300

    Add files via upload

---
+++ Intro to APIs/Member.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSC_AUB_Intro_to_APIs
+{
+    class Member
+    {
+        public int id;
+        public string name;
+        public string favFood;
+        public string hobby;
+        
+    }
+}
